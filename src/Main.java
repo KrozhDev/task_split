2fb0e32ac18078989e71a990a0d0fb1b398005f1
KrozhDev <roko.mdn@gmail.com>
2023-09-26 19:26:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**
+         * код, с помощью которого можно разделить строку на части
+         */
+
+        String string = "Split me into the parts";
+
+        String splitSymbol = " ";
+
+        String[] splittedString = string.split(splitSymbol);
+
+        for (String word: splittedString) {
+            System.out.print(word + ";");
+        }
+
+
+    }
+}
